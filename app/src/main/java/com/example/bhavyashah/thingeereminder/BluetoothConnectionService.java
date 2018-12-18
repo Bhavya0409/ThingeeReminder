9158d63a607e60896735232b7b643e6190dcf579
Bhavya Shah <bhavyashah@Bhavyas-MBP.home>
2018-12-17 20:36:04 -0500

    added restart receiver to restart service when android system closes it

--- app/src/main/java/com/example/bhavyashah/thingeereminder/BluetoothConnectionService.java
+++ app/src/main/java/com/example/bhavyashah/thingeereminder/BluetoothConnectionService.java
@@ -1,15 +1,23 @@
 package com.example.bhavyashah.thingeereminder;
 
+import android.app.Notification;
 import android.app.Service;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
 public class BluetoothConnectionService extends Service {
 
     private BluetoothConnectionReceiver bluetoothConnectionReceiver = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        startForeground(1, new Notification());
+    }
 
     @Nullable
     @Override
@@ -18,23 +26,26 @@ public class BluetoothConnectionService extends Service {
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
+    public void onDestroy() {
+        super.onDestroy();
 
+        SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.THINGEE_REMINDER_SHARED_PREFS, MODE_PRIVATE);
+        if (sharedPreferences.getBoolean(MainActivity.REMINDER_STATUS, true)) {
+            // If the reminder status was set to true, that means it wasn't the user that called
+            // onDestroy, but rather the android system. In this case, restart the service
+            Intent restartService = new Intent(this, RestartReceiver.class);
+            sendBroadcast(restartService);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
         intentFilter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
 
         bluetoothConnectionReceiver = new BluetoothConnectionReceiver();
         registerReceiver(bluetoothConnectionReceiver, intentFilter);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        if (bluetoothConnectionReceiver != null) {
-            unregisterReceiver(bluetoothConnectionReceiver);
-        }
+        return START_STICKY;
     }
 }
--- app/src/main/java/com/example/bhavyashah/thingeereminder/MainActivity.java
+++ app/src/main/java/com/example/bhavyashah/thingeereminder/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.bhavyashah.thingeereminder;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.reminder_button) Button reminderButton;
     @BindView(R.id.reminder_toggle_text) TextView enabledTextView;
 
+    Intent bluetoothConnectionService;
+    SharedPreferences sharedpreferences;
+
+    public static final String THINGEE_REMINDER_SHARED_PREFS = "THINGEE_REMINDER_SHARED_PREFS" ;
+    public static final String REMINDER_STATUS = "REMINDER_STATUS";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,21 +33,40 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         enabledTextView.setText(isMyServiceRunning(BluetoothConnectionService.class) ?
                 getString(R.string.enabled) : getString(R.string.disabled));
+
+        bluetoothConnectionService = new Intent(MainActivity.this, BluetoothConnectionService.class);
+
+        sharedpreferences = getSharedPreferences(THINGEE_REMINDER_SHARED_PREFS, Context.MODE_PRIVATE);
+
+        if (!sharedpreferences.contains(REMINDER_STATUS)) {
+            sharedpreferences.edit().putBoolean(REMINDER_STATUS, false).apply();
+        }
     }
 
     @OnClick(R.id.reminder_button)
     public void onClick(View v) {
-        Intent bluetoothConnectionService = new Intent(MainActivity.this, BluetoothConnectionService.class);
         if (isMyServiceRunning(BluetoothConnectionService.class)) {
+            // Was enabled, so now disable it
+            // Need commit since this needs to all be done synchronously
+            sharedpreferences.edit().putBoolean(REMINDER_STATUS, false).commit();
             stopService(bluetoothConnectionService);
             enabledTextView.setText(getString(R.string.disabled));
         } else {
+            // Was disabled, so now enable it
+            // Need commit since this needs to all be done synchronously
+            sharedpreferences.edit().putBoolean(REMINDER_STATUS, true).commit();
             startService(bluetoothConnectionService);
             enabledTextView.setText(getString(R.string.enabled));
         }
     }
 
-    private boolean isMyServiceRunning(Class<?> serviceClass) {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(bluetoothConnectionService);
+    }
+
+    public boolean isMyServiceRunning(Class<?> serviceClass) {
         ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
             if (serviceClass.getName().equals(service.service.getClassName())) {
